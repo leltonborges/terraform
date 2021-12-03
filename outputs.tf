51927e14b74e8958af755780ed0ce453b8101466
Lelton Borges <leltondev@gmail.com>
2021-12-03 14:29:25 -0300

    v2.1 output

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,11 @@ output "dev5" {
   value = aws_instance.dev5.public_ip
 }
 output "dev" {
-#   value = aws_instance.dev[*].public_ip
-    value = {
-        for k, v in aws_instance.dev : k => v.public_ip
+  #   value = aws_instance.dev[*].public_ip
+  value = {
+    for k, v in aws_instance.dev : k => {
+      id = v.id
+      ip = v.public_ip
     }
+  }
 }
