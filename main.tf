aedef1b88bc578308f2212e5613abd23ea0ce484
Lelton Borges <leltondev@gmail.com>
2021-12-03 12:43:53 -0300

    Mult provider and dynamodb

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+provider "aws" {
+  alias = "us-east-2"
+  version = "~> 2.0"
+  region  = "us-east-2"
+}
+
 resource "aws_instance" "dev" {
   count         = 3
   ami           = "ami-083654bd07b5da81d"
@@ -37,3 +43,16 @@ resource "aws_instance" "dev5" {
   vpc_security_group_ids = ["${aws_security_group.acess-ssh.id}"]
 }
 
+resource "aws_instance" "dev6" {
+  provider = "aws.us-east-2"
+  ami           = "ami-002068ed284fb165b"
+  instance_type = "t2.micro"
+  key_name      = "terraform"
+  tags = {
+    Name = "dev6"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acess-ssh-us-east-2.id}"]
+  depends_on = [
+    aws_dynamodb_table.dynamodb-dev
+  ]
+}
--- security-group.tf
+++ security-group.tf
@@ -7,7 +7,23 @@ resource "aws_security_group" "acess-ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["191.220.49.20/32"]
+  }
+  tags = {
+    Name = "ssh"
+  }
+}
+
+resource "aws_security_group" "acess-ssh-us-east-2" {
+  provider    = "aws.us-east-2"
+  name        = "acess-ssh"
+  description = "Acesso ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["191.220.49.20/32"]
   }
   tags = {
     Name = "ssh"
